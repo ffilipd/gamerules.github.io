4c711248fed757aa89d2a59116af5498bc0027b1
ffilipd <filipdahlskog@gmail.com>
2021-07-11 18:40:41 +0300

    updates

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const gameRules = "https://ffilipd.github.io/gamerules.github.io/gamerules.txt"
+
 $("#okbtn").click(function () {
-    axios.get("https://catfact.ninja/fact")
+    axios.get(gameRules)
     .then(function (res) {
-        console.log(res);
+        console.log(res.data);
     })
 })
+
+$("#search-input").change(function () {
+    let searchKey = $("#search-input").val();
+    console.log(searchKey);
+
+    $.get(gameRules, function (res) {
+        let result = (res.data.indexOf(searchKey));
+        console.log(res[result])
+    })
+})  
